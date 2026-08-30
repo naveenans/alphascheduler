b085339298b74adfb261dd6f829deb9364d45e5d
Navee <54275823+naveenans@users.noreply.github.com>
2026-08-30 21:21:23 +0530

    Initialize persistent finance store in v0.2

--- AlphaPlanner/app/src/main/java/com/alphaplanner/app/MainActivity.kt
+++ AlphaPlanner/app/src/main/java/com/alphaplanner/app/MainActivity.kt
@@ -3,12 +3,14 @@ package com.alphaplanner.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.alphaplanner.app.data.FinanceStore
 import com.alphaplanner.app.ui.AlphaPlannerApp
 import com.alphaplanner.app.ui.theme.AlphaPlannerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FinanceStore.init(applicationContext)
         setContent {
             AlphaPlannerTheme { AlphaPlannerApp() }
         }
